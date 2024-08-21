b8e6e41f47bab0d6db46c789471d46489a7d0749
aksu <Scyzhe@hotmail.com>
2024-08-21 16:18:29 +0300

    uibuild

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static('dist'))
+
 morgan.token('body', req => {
     return JSON.stringify(req.body)
 })
@@ -48,11 +50,6 @@ let spots = [
     }
 ]
   
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
 
 app.get('/api/spots', (request, response) => {
     response.json(spots)
